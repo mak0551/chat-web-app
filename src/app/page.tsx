7927dd1a1a08c06e8592f112a3b345cc6a204f99
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2024-10-16 20:35:26 +0530

    changes in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { main } from "framer-motion/client";
+import React from "react";
 
 export default function page() {
   return (
-    <div >
-      helllo
-    </div>
-  )
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">nextjs drill</h1>
+    </main>
+  );
 }
