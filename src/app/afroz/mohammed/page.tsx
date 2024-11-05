745bf5b082b4c63a96f8ec904d8e04ed88791c5e
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2024-11-05 19:11:03 +0530

    nested routing

---
+++ src/app/afroz/mohammed/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='bg-white'>
+      mohammed
+    </div>
+  )
+}
+
+export default index
---
+++ src/app/afroz/page.tsx
@@ -0,0 +1,3 @@
+export default function(){
+    return <h1 className="bg-white">hello afroz</h1>
+}
