7eb6a468e4255095488cc1e0a9d14718b46b9ac0
Mohammed Ahmed Khan <mohammedahmedkhan551@gmail.com>
2025-02-23 18:05:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function page() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">nextjs drill</h1>
+      <h1 className="text-2xl text-center">Nextjs drill</h1>
     </main>
   );
 }
