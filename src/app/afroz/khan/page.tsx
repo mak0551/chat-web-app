7b597e84fb8bcde0645f632edb9fed8fef21ad07
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-07-14 18:50:08 +0530

    page

--- src/app/afroz/khan/page.tsx
+++ src/app/afroz/khan/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
-  return (
-    <div className='bg-white'>
-      khan khan khan khan 
-    </div>
-  )
+  return <div className="bg-white">helloe </div>;
 }
